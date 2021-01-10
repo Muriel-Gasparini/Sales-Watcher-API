f76b97ec35279cc95603754fd351450e1cba85bf
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-09 21:49:39 -0300

    fix: fix query param error, and return correct account

--- src/database/repositories/AdminRepository.js
+++ src/database/repositories/AdminRepository.js
@@ -4,9 +4,9 @@ class AdminRepository {
 
   static async setAdmin (admin) {
     try {
-      const adminAccountExists = await this.getAdmin({ login: admin.login })
+      const adminRequested = await this.getAdmin({ queryParams: { login: admin.login } })
 
-      if (adminAccountExists.length === 1) return { adminAlreadyExists: true }
+      if (!adminRequested.accountNotFound) return { adminAlreadyExists: true }
 
       await Admin.create(admin)
 
@@ -16,10 +16,24 @@ class AdminRepository {
     }
   }
 
-  static async getAdmin (searchParams) {
+  static async getAdmin (searchParams = { queryParams: null, getPassword: null }) {
     try {
-      return await Admin.find(searchParams || null)
-    } catch (error) {
+      let account =  searchParams.getPassword ? await Admin.find(searchParams.queryParams || null).select('+password') : await Admin.find(searchParams.queryParams || null)
+
+      if (account.length < 1) return { accountNotFound: true }
+
+      account = account[0]
+
+      account = {
+        id: account._id,
+        name: account.name,
+        login: account.login,
+        password: searchParams.getPassword ? account.password : null,
+        deleteAdmin: account.deleteAdmin
+      }
+
+      return { accountNotFound: false, account }
+    } catch (e) {
       throw new Error('Error while get admin')     
     }
   }
