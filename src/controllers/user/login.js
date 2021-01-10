6c26f917e91e3ef1362fff34b24374d190660b90
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-09 23:01:20 -0300

    refactor: add body validation and clean the code

---
+++ src/controllers/user/login.js
@@ -0,0 +1,28 @@
+import UserRepository from '../../database/repositories/UserRepository'
+import Bcrypt from '../../utils/bcrypt'
+import Jwt from '../../utils/jwt'
+import BodyValidation from '../../utils/bodyValidation/user/login'
+
+export default async (req, res) => {
+  try {
+    const { login, password } = req.body
+
+    const invalidBodyErrorMessage = await BodyValidation({ login, password })
+
+    if (invalidBodyErrorMessage) return res.status(400).json(invalidBodyErrorMessage)
+
+    const user = await UserRepository.getUser({ queryParams: { login }, getPassword: true })
+
+    if (user.accountNotFound) return res.status(400).json({ error: 'This account does not exists' })
+
+    const match = await Bcrypt.compare(password, user.account.password)
+
+    if (!match) return res.status(400).json({ error: 'Invalid Credentials' })
+
+    const token = Jwt.generateToken({ id: user.account.id })
+
+    res.status(200).json({ accountId: user.account.id, token })
+  } catch (error) {
+    res.status(500).json({ error: 'An error ocurred while trying login' })
+  }
+}
--- src/routes/no-auth.js
+++ src/routes/no-auth.js
@@ -3,12 +3,13 @@ const router = Router()
 
 import AddAdminController from '../controllers/admin/create-account'
 import LoginAdminController from '../controllers/admin/login'
+import LoginUserController from '../controllers/user/login'
 
 const loginController = require('../controllers/loginController')
 
 router.post('/login/admin', LoginAdminController)
 
-router.post('/login/user', loginController.user)
+router.post('/login/user', LoginUserController)
 
 router.post('/admin/account', AddAdminController)
 
