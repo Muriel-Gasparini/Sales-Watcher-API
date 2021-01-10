4836ebc4632ef91d49ce528a1d1c85384f9ff7e6
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-10 16:17:45 -0300

    feat: create validation token method in the jwt class

--- src/utils/jwt/index.js
+++ src/utils/jwt/index.js
@@ -1,10 +1,28 @@
-import { sign } from 'jsonwebtoken'
+import { sign, verify } from 'jsonwebtoken'
+import envs from '../../config/envs'
 
 export default class {
 
-  static generateToken (payload) {
+  static generateToken(payload) {
     const token = sign(payload, process.env.SECRET, { expiresIn: '8h' })
-
     return `Bearer ${token}`
+  }
+
+  static async isValidToken(token) {
+    try {
+      const resultToDecodeJwt = await verify(token, envs.jwtSecret, (error, payload) => {
+        const errors = [
+          { testToError: /'expired/.test(error), errorMessage: 'This jwt is expired' },
+          { testToError: error, errorMessage: 'This jwt is invalid' }
+        ]
+        for (const x in errors) {
+          if (errors[x].testToError) return { isValidToken: false, errorMessage: errors[x].errorMessage }
+        }
+        return { isValidToken: true, payload: payload }
+      })
+      return resultToDecodeJwt
+    } catch (e) {
+      throw new Error('An error ocurred while trying decode JWT')
+    }
   }
 }
