04ec01af3c50e9bbccc52a8a40741d6201f082f1
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-10 16:15:40 -0300

    refactor: remove old import

--- src/routes/no-auth.js
+++ src/routes/no-auth.js
@@ -5,8 +5,6 @@ import AddAdminController from '../controllers/admin/create-account'
 import LoginAdminController from '../controllers/admin/login'
 import LoginUserController from '../controllers/user/login'
 
-const loginController = require('../controllers/loginController')
-
 router.post('/login/admin', LoginAdminController)
 
 router.post('/login/user', LoginUserController)
