29978c7b9f43f542bb63cc51214ebf262fbb7900
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-10 16:30:31 -0300

    refactor: move post controller to a crud file

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,26 +1,5 @@
 const Admin = require('../database/models/Admin')
 
-exports.create = async (req, res) => {
-
-  const { name, login, password } = req.body
-
-  try {
-
-    await Admin.create({
-      name,
-      login,
-      password,
-      deleteAdmin: false
-    })
-
-    res.status(201).json({ message: 'The administrator was created successfully' })
-
-  } catch (error) {
-
-    res.status(500).json({ error: 'An error occurred while creating the administrator' })
-  }
-}
-
 exports.delete = async (req, res) => {
 
   const { adminID } = req.body
