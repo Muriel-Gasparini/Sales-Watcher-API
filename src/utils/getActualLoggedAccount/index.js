3ae94635a6a0a78e723bc7f3ac00f3579d5d2847
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-10 22:04:49 -0300

    refactor: move getLoggedAccount to a util method

---
+++ src/utils/getActualLoggedAccount/index.js
@@ -0,0 +1,14 @@
+export default async (req) => {
+  const _id = req.accountId
+
+  const [userAccount, adminAccount] = await Promise.all([
+    UserRepository.getUser({ queryParams: { _id } }),
+    AdminRepository.getAdmin({ queryParams: { _id } })
+  ])
+  
+  if (userAccount.accountNotFound && adminAccount.accountNotFound) throw new Error('Error while get admin and user account')
+
+  const loggedAccount = userAccount.accountNotFound ? adminAccount.account[0] : userAccount.account[0]
+
+  return loggedAccount
+}
